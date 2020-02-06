d86df10f57f411b5690a05f9726688b025e449d6
balaji sathyamurthy <60360305+bsathyamur@users.noreply.github.com>
2020-02-05 22:12:50 -0800

    Add files via upload

---
+++ MP1Node.h
@@ -0,0 +1,96 @@
+/**********************************
+ * FILE NAME: MP1Node.cpp
+ *
+ * DESCRIPTION: Membership protocol run by this Node.
+ * 				Header file of MP1Node class.
+ **********************************/
+
+#ifndef _MP1NODE_H_
+#define _MP1NODE_H_
+
+#include "stdincludes.h"
+#include "Log.h"
+#include "Params.h"
+#include "Member.h"
+#include "EmulNet.h"
+#include "Queue.h"
+
+/**
+ * Macros
+ */
+#define TREMOVE 20
+#define TFAIL 5
+#define TCALL 2
+
+/*
+ * Note: You can change/add any functions in MP1Node.{h,cpp}
+ */
+
+/**
+ * Message Types
+ */
+enum MsgTypes{
+    JOINREQ,
+    JOINREP,
+    RECVCALL
+};
+
+/**
+ * STRUCT NAME: MessageHdr
+ *
+ * DESCRIPTION: Header and content of a message
+ */
+typedef struct MessageHdr {
+	enum MsgTypes msgType;
+}MessageHdr;
+
+/**
+ * CLASS NAME: MP1Node
+ *
+ * DESCRIPTION: Class implementing Membership protocol functionalities for failure detection
+ */
+class MP1Node {
+private:
+	EmulNet *emulNet;
+	Log *log;
+	Params *par;
+	Member *memberNode;
+	char NULLADDR[6];
+
+public:
+	MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
+	Member * getMemberNode() {
+		return memberNode;
+	}
+	int recvLoop();
+	static int enqueueWrapper(void *env, char *buff, int size);
+	void nodeStart(char *servaddrstr, short serverport);
+	int initThisNode(Address *joinaddr);
+	int introduceSelfToGroup(Address *joinAddress);
+	int finishUpThisNode();
+	void nodeLoop();
+	void checkMessages();
+	bool recvCallBack(void *env, char *data, int size);
+	void nodeLoopOps();
+	int isNullAddress(Address *addr);
+	Address getJoinAddress();
+	void initMemberListTable(Member *memberNode);
+	void printAddress(Address *addr);
+	virtual ~MP1Node();
+
+    bool pingRandomNeighbour();
+    char* CreateDataMsg(char *buffer);
+    Address GetNodeAddressFromIdAndPort(int id, short port);  
+    int GetIDFromAddress(Address *sourceAddr);
+    int GetPortFromAddress(Address *sourceAddr);    
+    void CheckFailure();
+
+	void PrintNodeInFile(string printTxt);
+    void PrintNodeInFile(string printTxt,int ID);
+    void PrintNodeInFile(string printTxt,Address *MemberAddr); 
+
+    void PrintMembershipTableInFile(Address *sourceAddr,int id,int port,int heartbeat,int Timestamp);	
+
+};
+
+#endif /* _MP1NODE_H_ */
